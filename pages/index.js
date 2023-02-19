8804bd8a42b20bf7d0947c5c52db7e7af663bbab
samueloso <ososamuel035@gmail.com>
2023-02-19 10:31:27 +0100

    feat: footer component and bottom bar

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import Footer from "@/features/common/modules/Footer";
 import Navigation from "@/features/common/modules/Navigation";
 
 export default function Home() {
-  return <Navigation />;
+  return (
+    <>
+      <Navigation />
+      <Footer />
+    </>
+  );
 }
