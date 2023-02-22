6ef2f04711c5dd542b517d1f2ad4f2e9150fe782
samueloso <ososamuel035@gmail.com>
2023-02-22 10:56:03 +0100

    feat: testimonials and swiper responsiveness

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import FeaturedProperties from "@/features/Home/components/FeaturedProperties";
 import HeroBanner from "@/features/Home/components/HeroBanner";
 import MeetTheTeam from "@/features/Home/components/MeetTheTeam";
 import Partners from "@/features/Home/components/Partners";
+import Testimonials from "@/features/Home/components/Testimonials";
 import DefaultLayout from "@/features/Layouts/DefaultLayout";
 
 export default function Home({ featuredProperties }) {
@@ -12,6 +13,7 @@ export default function Home({ featuredProperties }) {
         <FeaturedProperties featuredProperties={featuredProperties} />
         <MeetTheTeam />
         <Partners />
+        <Testimonials />
       </DefaultLayout>
     </>
   );
